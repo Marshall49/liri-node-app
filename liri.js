20659abb4cb096753fd044ccc0f9dc891bba4061
Malcolm Strong <strong.malcolm@yahoo.com>
2017-07-18 22:02:54 -0400

    finished the spotify

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-// var Movie = require('request');
+var Movie = require('request');
 
 var keys = require("./keys.js");
 
@@ -46,7 +46,7 @@ if (process.argv[2] === "spotify-this-song") {
     }
   }
 
-  var query = { type: 'track', query: songTitle };
+  var query = { type: 'track', query: songTitle , limit: 20 };
 
   spotify.search(query, function(err, data) {
     if (err) {
@@ -57,54 +57,26 @@ if (process.argv[2] === "spotify-this-song") {
       for (var i = 0; i < songs.length; i++) {
         // console.log(i);
         console.log("album: " + songs[i].album.name);
-        console.log("artist(s): " + songs[i].artists);
-
-        var artist =  songs[i].artists
-       for (var i = 0; i < artist.length; i++) {
-        //  console.log(i);
-         console.log(artist[i].name);
-    // console.log(data);
-    }
-  }
+        console.log("artist(s): " + songs[i].artists[0].name);
+        console.log("\n------------\n");
+        console.log(songs[i].name);
+      // var artist =  songs[i].artists;
+      //  for (var i = 0; i < artist.length; i++) {
+      //    console.log(artist[i].name);
+        //  console.log(songs[i].artists.name);
+        // console.log(data);
+        // }
+      }
 
     });
-}
-
-    // var nodeArgs = process.argv;
-    //
-    // // Create an empty variable for holding the movie name
-    // var movieName = "";
-    //
-    // // Loop through all the words in the node argument
-    // // And do a little for-loop magic to handle the inclusion of "+"s
-    // for (var i = 2; i < nodeArgs.length; i++) {
-    //
-    //   if (i > 2 && i < nodeArgs.length) {
-    //
-    //     movieName = movieName + "+" + nodeArgs[i];
-    //
-    //   }
-    //
-    //   else {
-    //
-    //     movieName += nodeArgs[i];
-    //
-    //   }
-    // }
-    //
-    // // Then run a request to the OMDB API with the movie specified
-    // var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-    //
-    // // This line is just to help us debug against the actual URL.
-    // console.log(queryUrl);
-    //
-    // request(queryUrl, function(error, response, body) {
-    //
-    //   // If the request is successful
-    //   if (!error && response.statusCode === 200) {
-    //
-    //     // Parse the body of the site and recover just the imdbRating
-    //     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    //     console.log("Release Year: " + JSON.parse(body).Year);
-    //   }
-    // });
+  }
+// } else if (process.argv[2] === "movie-this") {
+//     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+//     console.log(queryUrl);
+//
+//     request(queryUrl, function(error, response, body) {
+//       if (!error && response.statusCode === 200) {
+//         console.log("Release Year: " + JSON.parse(body).Year);
+//       }
+//     })
+// }
